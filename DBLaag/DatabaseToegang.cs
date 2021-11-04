826bf340285f565a4f72641d46b4b26b5d833c35
FelixvL <digitaleservice@gmail.com>
2021-11-04 10:37:17 +0100

    de dblaag toegevoegd

---
+++ DBLaag/DatabaseToegang.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBLaag
+{
+    public class DatabaseToegang : DbContext
+    {
+        public DatabaseToegang(DbContextOptions options) : base(options) { }
+        public DbSet<Product> producten { get; set; }
+    }
+}
---
+++ DBLaag/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBLaag
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public int Prijs { get; set; }
+        public string Specificaties { get; set; }
+    }
+}
