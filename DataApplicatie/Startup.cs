b5095db57e541e0875b78f5ca8fd3874ee20d7b4
FelixvL <digitaleservice@gmail.com>
2021-11-04 10:36:50 +0100

    update voor database selectie

--- DataApplicatie/Startup.cs
+++ DataApplicatie/Startup.cs
@@ -1,6 +1,8 @@
+using DBLaag;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace DataApplicatie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<DatabaseToegang>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
