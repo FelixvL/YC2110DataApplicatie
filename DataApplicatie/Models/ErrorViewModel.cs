93d638351f83ed154c875216bec99b7db4057f77
FelixvL <digitaleservice@gmail.com>
2021-11-04 09:39:03 +0100

    eerste commit

---
+++ DataApplicatie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataApplicatie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
